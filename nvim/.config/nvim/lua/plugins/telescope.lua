3d9b73030f96e377499ddcd4e75da55e9dbdf952
Gabriel Borges <gabriel.borges265@gmail.com>
2026-04-04 14:39:53 -0300

    Altera os atalhos do Telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,11 +3,8 @@ return {
     branch = '0.1.x',
     dependencies = { 'nvim-lua/plenary.nvim' },
     keys = {
-        { "<leader>f",  function() require("telescope.builtin").find_files() end,          desc = "Find files" },
-        { "<leader>b",  function() require("telescope.builtin").buffers() end,             desc = "Find buffers" },
-        { "<leader>sg", function() require("telescope.builtin").live_grep() end,           desc = "Search by Grep" },
-        { "gr",         function() require("telescope.builtin").lsp_references() end,      desc = "Go to References" },
-        { "gi",         function() require("telescope.builtin").lsp_implementations() end, desc = "Go to Implementation" },
-        { "<leader>sd", function() require("telescope.builtin").diagnostics() end,         desc = "Search Diagnostics" },
+        { "<leader>f", function() require("telescope.builtin").find_files() end, desc = "Find files" },
+        { "<leader>b", function() require("telescope.builtin").buffers() end,    desc = "Find buffers" },
+        { "<leader>g", function() require("telescope.builtin").live_grep() end,  desc = "Search by Grep" },
     },
 }
