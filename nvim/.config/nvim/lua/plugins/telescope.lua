6962c237fca8a70adc11f4c1b5e1c000fd7e2b61
Gabriel Borges <gabriel.borges265@gmail.com>
2026-04-05 12:59:03 -0300

    Aciona atalho para telescope

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -6,5 +6,6 @@ return {
         { "<leader>f", function() require("telescope.builtin").find_files() end, desc = "Find files" },
         { "<leader>b", function() require("telescope.builtin").buffers() end,    desc = "Find buffers" },
         { "<leader>g", function() require("telescope.builtin").live_grep() end,  desc = "Search by Grep" },
+        { "<leader>s", function() require("telescope.builtin").git_status() end, desc = "Search by Grep" },
     },
 }
