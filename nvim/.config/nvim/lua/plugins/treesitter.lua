c7e48c4041cc09bf63eb0e92cc965a3d31cb5f4d
Gabriel Borges <gabriel.borges265@gmail.com>
2026-04-28 22:55:24 -0300

    Add c# and tsx treesitter parsers

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,32 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  main = "nvim-treesitter.configs",
-  opts = {
-    ensure_installed = {
-        -- Neovim essentials
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    main = "nvim-treesitter.configs",
+    opts = {
+        ensure_installed = {
+            -- Neovim essentials
+            "lua",
+            "vim",
+            "vimdoc",
+            "query",
 
-        -- Work use
-        "javascript",
-        "typescript",
-        "html",
-        "css",
+            -- Work use
+            "c_sharp",
+            "tsx",
+            "javascript",
+            "typescript",
+            "html",
+            "css",
 
-        -- Web extra
-        "json",
-        "markdown",
+            -- Web extra
+            "json",
+            "markdown",
 
-        -- Not use too much
-        "c",
-        "cpp",
-        "python",
-    },
-    highlight = { enable = true }
-  }
+            -- Not use too much
+            "c",
+            "cpp",
+            "python",
+        },
+        highlight = { enable = true }
+    }
 }
