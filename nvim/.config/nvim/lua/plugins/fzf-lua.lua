acd679e73415544a3e4938fa965dce68ed651938
Gabriel Borges <gabriel.borges265@gmail.com>
2026-05-16 21:29:58 -0300

    feat: add fzf-lua plugin

---
+++ nvim/.config/nvim/lua/plugins/fzf-lua.lua
@@ -0,0 +1,21 @@
+return {
+    "ibhagwan/fzf-lua",
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    -- or if using mini.icons/mini.nvim
+    -- dependencies = { "nvim-mini/mini.icons" },
+    ---@module "fzf-lua"
+    ---@type fzf-lua.Config|{}
+    ---@diagnostic disable: missing-fields
+    opts = {},
+    ---@diagnostic enable: missing-fields
+    keys = {
+        { "<leader>f", "<cmd>FzfLua files<cr>",                 desc = "Find File" },
+        { "<leader>g", "<cmd>FzfLua live_grep<cr>",             desc = "Live Grep" },
+        { "<leader>b", "<cmd>FzfLua buffers<cr>",               desc = "Find Buffers" },
+        { "<leader>h", "<cmd>FzfLua help_tags<cr>",             desc = "Search help" },
+        { "<leader>s", "<cmd>FzfLua git_status<cr>",            desc = "Git status" },
+        { "<leader>d", "<cmd>FzfLua diagnostics_workspace<cr>", desc = "Diagnostics" },
+        { "<leader>l", "<cmd>FzfLua lsp_finder<cr>",            desc = "LSP finder" },
+    }
+}
